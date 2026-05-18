6dc4842201d2f3bad5e666c302ae085ea02c8c8b
pegucas <pedrogudincastanho@gmail.com>
2026-05-18 13:38:52 -0300

    Update menu.py

--- listas python e ac/menu.py
+++ listas python e ac/menu.py
@@ -1,4 +1,5 @@
 def Menu():
+    print("-" * 45)
     print("Escolha uma opção abaixo:")
     print("1 - Cadastrar um novo jogador")
     print("2 - Mostrar jogadores cadastrados")
@@ -6,24 +7,6 @@ def Menu():
     print("4 - Pesquisar jogador")
     print("5 - Alterar uma informação")
     print("6 - Remover um jogador")
-    print("7 - Mostrar estatisticas gerais")
+    print("7 - Mostrar estatisticas gerais de todos")
     print("8 - Encerrar programa")
-
-def Ifs():
-    EscolhaMenu = int(input("Qual sera a opção?: "))
-    if EscolhaMenu == 1:
-        print("OK")
-    elif EscolhaMenu == 2:
-        print("Beleza")
-    elif EscolhaMenu == 3:
-        print("joia")
-    elif EscolhaMenu == 4:
-        print("Ta")
-    elif EscolhaMenu == 5:
-        print("Kay")
-    elif EscolhaMenu == 6:
-        print("Ta bom")
-    elif EscolhaMenu == 7:
-        print("Sim")
-    else:
-        print("Vazando daqui...")
+    print("-" * 45)
