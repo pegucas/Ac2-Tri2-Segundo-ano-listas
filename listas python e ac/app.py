4a9331774939b37b6d49ed2783afa33fee81a844
pegucas <pedrogudincastanho@gmail.com>
2026-05-18 13:37:34 -0300

    Melhoria e arrumação

--- listas python e ac/app.py
+++ listas python e ac/app.py
@@ -1,4 +1,6 @@
-from menu import Menu, Ifs
+from menu import Menu
+from functions import Cadastro, Cadastrados, relatorioCompleto, PesquisaJogador, AlterarInfo, Remover, Estatisticas
+
 EscolhaMenu = 0
 
 while EscolhaMenu != 8:
@@ -7,18 +9,33 @@ while EscolhaMenu != 8:
     EscolhaMenu = int(input("Qual sera a opção?: "))
 
     if EscolhaMenu == 1:
+        print("-" * 45)
         print("OK")
+        Cadastro()
     elif EscolhaMenu == 2:
+        print("-" * 45)
         print("Beleza")
+        Cadastrados()
     elif EscolhaMenu == 3:
+        print("-" * 45)
         print("joia")
+        relatorioCompleto()
     elif EscolhaMenu == 4:
+        print("-" * 45)
         print("Ta")
+        PesquisaJogador()
     elif EscolhaMenu == 5:
+        print("-" * 45)
         print("Kay")
+        AlterarInfo()
     elif EscolhaMenu == 6:
+        print("-" * 45)
         print("Ta bom")
+        Remover()
     elif EscolhaMenu == 7:
+        print("-" * 45)
         print("Sim")
+        Estatisticas()
     else:
-        print("Vazando daqui...")
+        print("-" * 45)
+        print("Encerrando programa...")
